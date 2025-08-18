670ad9b963e5774a1f72e768e0ab1a47d86a94f8
Yikang <starowen129@gmail.com>
2025-08-18 13:52:16 +0200

    Testing Github Pages

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,12 @@
 export const siteConfig = {
-  name: "Ryan Fitzgerald",
-  title: "Senior Software Engineer",
-  description: "Portfolio website of Ryan Fitzgerald",
+  name: "Yikang Chen",
+  title: "Junior Firmware/Software Engineer",
+  description: "Portfolio website of Yikang Chen",
   accentColor: "#1d4ed8",
   social: {
-    email: "your-email@example.com",
+    email: "yikang099@gmail.com",
     linkedin: "https://linkedin.com/in/yourprofile",
-    twitter: "https://x.com/rfitzio",
-    github: "https://github.com/RyanFitzgerald",
+    github: "https://github.com/Yikch",
   },
   aboutMe:
     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem quos asperiores nihil consequatur tempore cupiditate architecto natus commodi corrupti quas quasi facere est, dignissimos odit nam veniam sapiente ut, vitae eligendi ipsum dolor, nostrum ullam impedit! Corrupti ratione mollitia temporibus necessitatibus, consectetur reiciendis recusandae id, dolorum quaerat, vero pariatur. Ratione!",
